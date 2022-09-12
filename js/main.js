3e071ad066730a7cc595f6433907171cda7bb978
vsalter <vsalter1@student.gsu.edu>
2022-09-11 23:02:04 -0400

    updated win logic. checks for horizontal, vertical, and diagonal winning combinations. Also checks for tie

--- js/main.js
+++ js/main.js
@@ -7,16 +7,16 @@ let turn = 1;
 let id;
 let winner;
 let board = [null, null, null, null, null, null, null, null, null];
-const winningCombos = [
-	["0", "1", "2"],
-	["3", "4", "5"],
-	["6", "7", "8"],
-	["0", "3", "6"],
-	["1", "4", "7"],
-	["2", "5", "8"],
-	["0", "4", "8"],
-	["2", "4", "6"]
-	];
+//const winningCombos = [
+//	["0", "1", "2"],
+//	["3", "4", "5"],
+//	["6", "7", "8"],
+//	["0", "3", "6"],
+//	["1", "4", "7"],
+//	["2", "5", "8"],
+//	["0", "4", "8"],
+//	["2", "4", "6"]
+//	];
 let player1moves = [];
 let player2moves = [];
 
@@ -65,50 +65,125 @@ function makeAmove(evt) {
 		return;
 	}
 	else {
-		board[id] = id;
 		if (turn === 1){
 			evt.target.style.backgroundColor = "red";
 			evt.target.innerHTML = "X";
-			id.toString();
-			checkIfWon(id, turn);
-			turn ++;	
+//			id.toString();
+			board[id] = 'X';
+			console.log(board);
+			checkWinner(turn);	
 		}else {
 			evt.target.style.backgroundColor = "lightBlue";
 			evt.target.innerHTML = "O";
-			id.toString();
-			checkIfWon(id, turn);
-			turn --;
+//			id.toString();
+			board[id] = 'O';
+			console.log(board);
+			checkWinner(turn);
 		}
 	}		
 }
 
+
+function checkWinner(num) {
+
+	if (num === 1) {
+		if ((hCheck(num)) || (vCheck(num)) || (dCheck())) {
+			alert('Player 1 is the winner');
+			return;
+		} else if (tCheck()) {
+			alert('Looks like we have a tie');
+			return;
+		} else {
+			turn++;
+			return;
+		}
+	} else {
+		if ((hCheck(num)) || (vCheck(num)) || (dCheck())) {
+			alert('Player 2 is the winner');
+			return;
+		} else if (tCheck()) {
+			alert('Looks like we have a tie');
+			return;
+		} else {
+			turn--;
+			return;
+		}
+	}
+};
+
+
+function hCheck(num) {
+	if (num === 1){
+		if (((board[0] === 'X') && (board[3] === 'X') && (board[6] === 'X')) || ((board[1] === 'X') && (board[4] === 'X') && (board[7] === 'X')) || ((board[2] === 'X') && (board[5] === 'X') && (board[8] === 'X'))) {
+			document.querySelectorAll(".square").forEach(function(square){
+				square.removeEventListener("click", makeAmove);
+			});
+			return true;
+		}
+	} else {
+		if (((board[0] === 'O') && (board[3] === 'O') && (board[6] === 'O')) || ((board[1] === 'O') && (board[4] === 'O') && (board[7] === 'O')) || ((board[2] === 'O') && (board[5] === 'O') && (board[8] === 'O'))) {
+			document.querySelectorAll(".square").forEach(function(square){
+				square.removeEventListener("click", makeAmove);
+			});
+			return true;
+		}
+	}
+}
+
+
+function vCheck(num) {
+	if (num === 1){
+		if (((board[0] === 'X') && (board[1] === 'X') && (board[2] === 'X')) || ((board[3] === 'X') && (board[4] === 'X') && (board[5] === 'X')) || ((board[6] === 'X') && (board[7] === 'X') && (board[8] === 'X'))) {
+			document.querySelectorAll(".square").forEach(function(square){
+				square.removeEventListener("click", makeAmove);
+			});
+			return true;
+		}
+	} else {
+		if (((board[0] === 'O') && (board[1] === 'O') && (board[2] === 'O')) || ((board[3] === 'O') && (board[4] === 'O') && (board[5] === 'O')) || ((board[6] === 'O') && (board[7] === 'O') && (board[8] === 'O'))) {
+			document.querySelectorAll(".square").forEach(function(square){
+				square.removeEventListener("click", makeAmove);
+			});
+			return true;
+		}
+	}
+}
+
+
+function dCheck(num) {
+	if (num === 1){
+		if (((board[0] === 'X') && (board[4] === 'X') && (board[8] === 'X')) || ((board[2] === 'X') && (board[4] === 'X') && (board[6] === 'X'))) {
+			document.querySelectorAll(".square").forEach(function(square){
+				square.removeEventListener("click", makeAmove);
+			});
+			return true;
+		}
+	} else {
+		if (((board[0] === 'O') && (board[4] === 'O') && (board[8] === 'O')) || ((board[2] === 'O') && (board[4] === 'O') && (board[6] === 'O'))) {
+			document.querySelectorAll(".square").forEach(function(square){
+				square.removeEventListener("click", makeAmove);
+			});
+			return true;
+		}
+	}
+}
+
+
+function tCheck(){
+	let sum = 0;
+	for (let a = 0; a < 9; a++){
+		if(board[a] === null){
+			sum++;
+		}	
+	}
+	if (sum > 0){
+		return false;
+	} else {
+		return true;
+	}
+}
+
+
 function restartGame() {
 	window.location.reload(true);
 };
-
-function checkIfWon(string, num) {
-	let match = 0;
-	if (num === 1) {
-		player1moves.push(string);
-		player1moves.forEach(function(){
-			if (winningCombos.includes(num)){
-				match++;
-			}
-			if (match === 3){
-				alert("Player 1 Wins!");
-			}
-		});
-	}
-	else {
-		player2moves.push(string);
-		player2moves.forEach(function(){
-			if (winningCombos.includes(num)){
-				match++;
-			}
-			if (match === 3){
-				alert("Player 2 Wins!");
-			}
-		});
-	}
-};
-
